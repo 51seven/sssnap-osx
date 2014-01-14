a24af4d521dee3f4facd4e17fcc9112841240145
plsr <christianpoplawski@gmail.com>
2014-01-14 03:51:07 +0100

    write sssnap directory if there ain’t one

--- sssnap/sssnap/Token.m
+++ sssnap/sssnap/Token.m
@@ -11,12 +11,36 @@
 
 @implementation Token
 
+//
+//  Writes token and Username to a file called token.txt
+//  Directory: /users/USER/sssnap/token.txt
+//  Format in token.txt: username:token
+//
 -(NSString *)writeToken:(NSString *)username and: (NSString *) token {
     
+    //  Build path
     NSString *sssnap = @"/sssnap/token.txt";
     NSString *path = [NSHomeDirectory() stringByAppendingString:sssnap];
     
-    NSString *content = [username stringByAppendingString:token];
+    //  Check if directory already exists
+    NSString *pathToFile = [NSHomeDirectory() stringByAppendingString:@"/sssnap"];
+    BOOL isDir = NO;
+    BOOL isFile = [[NSFileManager defaultManager] fileExistsAtPath:pathToFile isDirectory:&isDir];
+    
+    if(isFile)
+    {
+        NSLog(@"Directory exists");
+    }
+    else
+    {
+        [ [ NSFileManager defaultManager ] createDirectoryAtPath: pathToFile withIntermediateDirectories: YES attributes: nil error: NULL ];
+        NSLog(@"Directory does not exist, created one");
+    }
+    
+    
+    //  Write username and token to file
+    NSString *aToken = [@":" stringByAppendingString:token];
+    NSString *content = [username stringByAppendingString:aToken];
     NSData *fileContents = [content dataUsingEncoding:NSUTF8StringEncoding];
     [[NSFileManager defaultManager] createFileAtPath:path
                                             contents:fileContents

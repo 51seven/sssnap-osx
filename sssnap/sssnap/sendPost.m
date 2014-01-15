29b7205e17853e2ce5330dd3a89d4eb01bc770b6
Sven Schiffer <svenschiffer92@gmail.com>
2014-01-15 19:44:42 +0100

    Added isValidToken-Method

--- sssnap/sssnap/sendPost.m
+++ sssnap/sssnap/sendPost.m
@@ -60,6 +60,61 @@
     return str;
 }
 
+// Authenticates against username and password. Returns unique user token if success.
+- (BOOL)isValidToken:(NSString *) username with:(NSString *) token {
+    printf("sending token authentification");
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setCachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
+    [request setHTTPShouldHandleCookies:NO];
+    [request setTimeoutInterval:30];
+    [request setHTTPMethod:@"POST"];
+    
+    NSMutableDictionary* _params = [[NSMutableDictionary alloc] init];
+    [_params setObject:[NSString stringWithFormat:@"%@", username] forKey:@"username"];
+    [_params setObject:[NSString stringWithFormat:@"%@", token] forKey:@"token"];
+    
+    NSString *BoundaryConstant = @"V2ymHFgaLs0Lac8qCaKO6jy";
+    NSURL *requestURL = [[NSURL alloc] initWithString:@"http://localhost:3000/api/auth_token"];
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", BoundaryConstant];
+    [request setValue:contentType forHTTPHeaderField: @"Content-Type"];
+    
+    NSMutableData *body = [NSMutableData data];
+    
+    // add params (all params are strings)
+    for (NSString *param in _params) {
+        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", BoundaryConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", param] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"%@\r\n", [_params objectForKey:param]] dataUsingEncoding:NSUTF8StringEncoding]];
+    }
+    
+    [body appendData:[[NSString stringWithFormat:@"--%@--\r\n", BoundaryConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    // setting the body of the post to the reqeust
+    [request setHTTPBody:body];
+    
+    // set the content-length
+    NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[body length]];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    
+    // set URL
+    [request setURL:requestURL];
+    
+    NSURLResponse *response = nil;
+    NSError *err = nil;
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&err];
+    NSString *str = [[NSString alloc] initWithBytes:[data bytes] length:[data length] encoding:NSUTF8StringEncoding];
+    NSLog(@"User-Token: %@",str);
+    
+    if([str isEqual:@"true"]) {
+        return YES;
+    }
+    else {
+        return NO;
+    }
+}
+
+
 
 // Sends an HTTP POST-Request
 // S. http://pastebin.com/R70NJMnQ
